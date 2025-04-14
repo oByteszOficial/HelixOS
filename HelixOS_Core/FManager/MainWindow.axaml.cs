e6bb023e7ab11ed7bc49639db32ca9b115248c9d
oByteszOficial <iplaqueiphones@gmailcom>
2025-04-14 01:13:04 -0300

    HelixOS first commit

---
+++ HelixOS_Core/FManager/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
